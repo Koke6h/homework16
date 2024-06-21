18a180c05dcd8c1af67b3a823722c4d3c077e78e
User <git@github.com:Koke6h/helloworld.git>
2024-06-21 21:42:05 +0400

    end hw15

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String login = "serzhanovd";
+        String password = "Daniil1234";
+        String confirmPassword = "Daniil1234";
+        AuthService.validate(login, password, confirmPassword );
+        boolean success = AuthService.validate(login, password, confirmPassword);
+        if (success) {
+            System.out.println("Проверка пройдена успешно");
+        } else {
+            System.out.println("Проверка не пройдена");
+        }
     }
 }
